1d153e583fb933a833cb1ad2b2129cbcd6c810a2
rkelmo <kelmette88@gmail.com>
2019-04-11 18:08:27 -0500

    test add good passing

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -26,12 +26,33 @@ public class TestGreeter {
 
    {
 
-      assertEquals(g.getName(),"");
 
-      assertEquals(g.sayHello(),"Hello!");
+      g.setName("Great Name");
+
+      assertEquals(g.getName(),"Great Name");
+
+      assertEquals(g.sayHello(),"Hello + Great Name!");
+
+      g.setName("A Better Name");
+
+      assertEquals(g.sayHello(),"Hello + A Better Name!");
+
+
 
    }
 
+/*
+   @Test
+   public void newtestRKGreeterFail() 
+
+   {
+
+      assertEquals(g.getName(),"fake name");
+
+      assertEquals(g.sayHello(),"Goodbye!");
+
+   }
+*/
 	
 
    @Before
