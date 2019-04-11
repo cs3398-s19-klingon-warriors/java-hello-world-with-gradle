9f51a2271bf1081f3693b4d2095cc543fb401da0
sfg11 <sfg11@txstate.edu>
2019-04-11 18:00:03 -0500

    added two tests!

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -63,3 +63,38 @@ public class TestGreeter {
    }
 
 }
+
+
+   @Test
+
+   public void test() 
+
+   {
+
+      g.setName("World");
+
+      assertEquals(g.getName(),"World");
+
+      assertEquals(g.sayHello(),"Hello World!");
+
+   }
+
+
+   // Assignment 16- Part 2a
+   @Test
+   public void newtest_SG_GreeterPass()
+   {
+      g.setName("RiIckkk");
+      assertEquals(g.getName(),"");
+      assertEquals(g.sayHello(),"Hello Rickyyy");
+   }
+   // Assignment 16 - Part 5a
+   @Test
+   public void newtest_SG_GreeterFail()
+   {
+      g.setName("Morty");
+      assertEquals(g.getName(),"(not) Morty");
+      assertEquals(g.sayHello(),"Hello (not) Morty!");
+   }
+
+}
