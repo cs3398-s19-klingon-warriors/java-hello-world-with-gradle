1aaca851463498d3a93ffbc87a720e72d3e6fb08
serena-marie <serena.gutierrez0@gmail.com>
2019-04-12 10:13:50 -0500

    GreeterPass
    Confirmed success in gradle

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -3,63 +3,36 @@ package hello;
 
 
 import static org.junit.Assert.*;
-
-
-
 import org.junit.Before;
-
 import org.junit.Test;
-
-
-
-
 
 public class TestGreeter {
 
-
-
    private Greeter g;
 
-	
-
    @Before
-
-   public void setUp() throws Exception 
-
-   {
-
+   public void setUp() throws Exception {
       g = new Greeter();
 
    }
 
-
-
    @Test
-
-   public void testGreeterEmpty() 
-
-   {
-
+   public void testGreeterEmpty() {
       assertEquals(g.getName(),"");
-
       assertEquals(g.sayHello(),"Hello!");
-
    }
-
-	
 
    @Test
-
-   public void testGreeter() 
-
-   {
-
+   public void testGreeter() {
       g.setName("World");
-
       assertEquals(g.getName(),"World");
-
       assertEquals(g.sayHello(),"Hello World!");
-
    }
 
+   @Test
+   public void InigoTestSGGreeterPass(){
+       g.setName("Inigo Montoya");
+       assertEquals(g.getName(), "Inigo Montoya");
+       assertEquals(g.sayHello(), "Hello Inigo Montoya!");
+   }
 }
